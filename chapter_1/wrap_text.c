ee6cea45fcb3734314ff2478846cf2f08ae29df2
Jesse Iberri <jesse.iberri@gmail.com>
2021-12-16 20:21:56 -0800

    Completed Ch.1 Exercises

--- chapter_1/wrap_text.c
+++ chapter_1/wrap_text.c
@@ -2,34 +2,87 @@
 #include <stdbool.h>
 
 #define MAXSIZE 1000
+#define DEBUG 0
+
 #define NO_SPACE 0
 #define SPACE    1
 
+int getScreenWidth();
 int getData(char cCollection[MAXSIZE][MAXSIZE]);
 
-void addCollection(char cCollection[MAXSIZE][MAXSIZE], int* iCollectionSize,
-                   char cString[], int* iStringSize);
+void addCollection(char cCollection[MAXSIZE][MAXSIZE], int *iCollectionSize,
+                   char cString[], int *iStringSize);
 
-void addChar(char cString[], int iStringSize, char cVal);
+void addChar(char cString[], int *iStringSize, char cVal);
 void clearString(char cString[], int* iStringSize);
+int stringLength (char cString[]);
 
 void main()
 {
     char cCollection[MAXSIZE][MAXSIZE];
     int iCollectionSize;
+    int iScreenWidth;
+
+    printf("Please enter screen width:\n\n");
+    iScreenWidth = getScreenWidth();
+
+    printf("\nScreen width: %d\n\n", iScreenWidth);
 
     printf("Please enter some text:\n\n");
-
     iCollectionSize = getData(cCollection);
-    printf("\nPrinting string collection:\n\n");
-    for (int i = 0; i < iCollectionSize; i++)
+
+    if (DEBUG)
     {
-        printf("String:\t%d\n", i);
-        for (int j = 0; j < MAXSIZE && cCollection[i][j] != '\0'; j++) {
-            printf("%c", cCollection[i][j]);
+        printf("\nPrinting string collection:\n\n");
+        for (int i = 0; i < iCollectionSize; i++)
+        {
+            printf("String:\t%d\n", i + 1);
+            for (int j = 0; j < MAXSIZE && cCollection[i][j] != '\0'; j++) {
+                printf("%c", cCollection[i][j]);
+            }
+            printf("\n");
         }
-        printf("\n");
     }
+
+    printf("\nWrapped Text:\n\n");
+    int iCurrLength = 0;
+    for (int i = 0; i < iCollectionSize; i++) {
+        // Here *cString is a pointer to the string array in the collection
+        // we are adding i to scale the y index of the two dimensional array
+        // and casting it to a pointer so we can deference it as a string
+        char *cString = *(cCollection + i);
+        int iLength = stringLength(cString);
+
+        if (iLength + iCurrLength > iScreenWidth) {
+            printf("\n");
+            iCurrLength = 0;
+        }
+
+        printf("%s", cString);
+        iCurrLength += iLength;
+
+        if (i + 1 < iCollectionSize &&
+            iCurrLength + 1 <= iScreenWidth) {
+            printf(" ");
+            iCurrLength++;
+        }
+    }
+    printf("\n");
+}
+
+int getScreenWidth()
+{
+    int iChar;
+    int iReturn = 0;
+    int iDefault = 10;
+
+    while ((iChar = getchar()) != EOF)
+    {
+        if (iChar >= '0' && iChar <= '9') iReturn = 10 * iReturn + (iChar - '0');
+        if (iChar == '\n' || iChar == '\t') break;
+    }
+    if (iReturn <= 0) iReturn = iDefault;
+    return iReturn;
 }
 
 int getData(char cCollection[MAXSIZE][MAXSIZE])
@@ -51,32 +104,19 @@ int getData(char cCollection[MAXSIZE][MAXSIZE])
         {
             iState = SPACE;
 
-            printf("closing string\n");
-            addChar(cString, iStringSize, '\0');
-            iStringSize++;
-            /*
-            cString[iStringSize] = '\0';
-            iStringSize++;
-            */
+            if (DEBUG) printf("closing string\n");
+            addChar(cString, &iStringSize, '\0');
 
-            printf("String: %s\n", cString);
-            printf("String Length:%d\n", iStringSize);
-
-            //addCollection(cCollection, &iCollectionSize, cString, &iStringSize);
-            for (int i = 0; i < iStringSize; i++)
+            if (DEBUG)
             {
-                cCollection[iCollectionSize][i] = cString[i];
-                if (cString[i] == '\0') break;
+                printf("String: %s\n", cString);
+                printf("String Length:%d\n", iStringSize);
             }
-            iCollectionSize++;
 
-            //clearString(cString, &iStringSize);
-            for (int i = 0; i < iStringSize; i++) {
-                cString[i] = '\0';
-            }
-            iStringSize = 0;
+            addCollection(cCollection, &iCollectionSize, cString, &iStringSize);
+            clearString(cString, &iStringSize);
 
-            //printf("String Count:%d\n", iCollectionSize);
+            if (DEBUG) printf("String Count:%d\n", iCollectionSize);
         } else
         if ((iChar == ' '  ||
              iChar == '\n' ||
@@ -87,40 +127,26 @@ int getData(char cCollection[MAXSIZE][MAXSIZE])
         {
             iState = NO_SPACE;
 
-            printf("adding char %c\n", iChar);
+            if (DEBUG) printf("adding char %c\n", iChar);
+
             char cTemp = (char)iChar;
-            addChar(cString, iStringSize, cTemp);
-            iStringSize++;
+            addChar(cString, &iStringSize, cTemp);
 
-            /*
-            cString[iStringSize] = iChar;
-            iStringSize++;
-            */
-
-            printf("String: %s\n", cString);
-            printf("String Length:%d\n", iStringSize);
+            if (DEBUG)
+            {
+                printf("String: %s\n", cString);
+                printf("String Length:%d\n", iStringSize);
+            }
         }
     }
-
-/*
-    printf("\nPrinting string collection:\n\n");
-    for (int i = 0; i < iCollectionSize; i++)
-    {
-        printf("String:\t%d\n", i);
-        for (int j = 0; j < MAXSIZE && cCollection[i][j] != '\0'; j++) {
-            printf("%c", cCollection[i][j]);
-        }
-        printf("\n");
-    }
-*/
 
     return iCollectionSize;
 }
 
 void addCollection(char cCollection[MAXSIZE][MAXSIZE],
-                   int* iCollectionSize,
+                   int *iCollectionSize,
                    char cString[],
-                   int* iStringSize)
+                   int *iStringSize)
 {
     for (int i = 0; i < *iStringSize; i++)
     {
@@ -128,13 +154,13 @@ void addCollection(char cCollection[MAXSIZE][MAXSIZE],
         if (cString[i] == '\0') break;
     }
 
-    *iCollectionSize++;
-    clearString(cString, iStringSize);
+    (*iCollectionSize)++;
 }
 
-void addChar(char cString[], int iStringSize, char cVal)
+void addChar(char cString[], int *iStringSize, char cVal)
 {
-    cString[iStringSize] = cVal;
+    cString[*iStringSize] = cVal;
+    (*iStringSize)++;
 }
 
 void clearString(char cString[], int* iStringSize)
@@ -145,4 +171,11 @@ void clearString(char cString[], int* iStringSize)
     }
 
     *iStringSize = 0;
+}
+
+int stringLength(char cString[])
+{
+    int iReturn;
+    for (iReturn = 0; cString[iReturn] != '\0'; iReturn++);
+    return iReturn;
 }
